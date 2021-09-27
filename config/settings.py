f21a644e00cc19eb51410addd7197db9b215d349
zuke <zuke0000@gmail.com>
2021-09-27 13:39:55 -0600

    updates for heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v_b7s_yw248u*1=4$@035r4v8!5(0@s&ajcoe!@rxxv^3-2yln'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
