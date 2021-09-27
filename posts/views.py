25f32da6328b9e00ba4f40dc362437cd77691ab7
zuke <zuke0000@gmail.com>
2021-09-27 13:13:17 -0600

    initial commit

---
+++ posts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'all_posts_list'
+# Create your views here.
